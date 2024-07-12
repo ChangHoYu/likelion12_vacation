e6dfe5de65f4bf0fdf0e3948011eb35ddea49739
changhoyu <tyu9817@naver.com>
2024-07-12 22:24:45 +0900

    fetch완료

--- http/4_axios_fetch/src/App.jsx
+++ http/4_axios_fetch/src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { fetchDeleteData } from './fetch/http';
 
 function App() {
+  console.log(fetchDeleteData());
   return <></>;
 }
 
--- http/4_axios_fetch/src/fetch/http.js
+++ http/4_axios_fetch/src/fetch/http.js
@@ -18,11 +18,102 @@ export async function fetchGetData() {
   const response = await fetch(url);
   const resData = await response.json();
 
+  // 이 코드가 필요한 이유
+  // 기본적으로 잘못된 url이나 요청을 보내도 State는 fufilled 즉 resolve함수를 호출해서 이행이됨.
+  // 단 잘못된 요청은 ok가 false로 되기에 if문의 !response.ok로 성공여부를 판단하고
+  // 에러 객체를 만든 후 throw하여 에러처리를 하는 것
+  // PromiseState는 rejected로 result는 에러 객체를 포함하고 있음.
   if (!response.ok) {
     const error = new Error(response.statusText || '데이터를 불러오는데 실패했습니다.');
     error.status = response.status;
     throw error;
   }
 
+  return resData;
+}
+
+export async function fetchPostData() {
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'fetch test',
+      body: 'hello world',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+  const resData = response.json();
+
+  if (!response.ok) {
+    const error = new Error(response.statusText || '데이터를 생성하는데 실패했습니다.');
+    error.status = response.status;
+    throw error;
+  }
+  // 우리가 요청한 데이터가 담겨있음.
+  return resData;
+}
+/**
+ * PATCH vs PUT
+ * The PUT method replaces all current representations of the target resource with the request payload. (MDN)
+ * The PUT method requests that the state of the target resource be created or replaced with the state defined by the representation enclosed in the request message content. (RFC)
+ */
+export async function fetchPatchData() {
+  const response = await fetch(url + '/1', {
+    method: 'PATCH',
+    body: JSON.stringify({
+      title: 'fetch test',
+    }),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error(response.statusText || '데이터를 변경하는데 실패했습니다.');
+    error.status = response.status;
+    throw error;
+  }
+
+  const resData = response.json();
+  return resData;
+}
+
+export async function fetchPutData() {
+  const response = await fetch(url + '/1', {
+    method: 'PUT',
+    body: JSON.stringify({
+      title: 'fetch test',
+      body: 'likelion_12 front_end_study',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error(response.statusText || '데이터를 변경하는데 실패했습니다.');
+    error.status = response.status;
+    throw error;
+  }
+
+  const resData = response.json();
+  return resData;
+}
+
+export async function fetchDeleteData() {
+  const response = await fetch(url + '/1', {
+    method: 'Delete',
+  });
+
+  if (!response.ok) {
+    const error = new Error(response.statusText || '데이터를 삭제하는데 실패했습니다.');
+    error.status = response.status;
+    throw error;
+  }
+
+  const resData = response.json();
   return resData;
 }
