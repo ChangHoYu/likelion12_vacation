7775c43d9d6b5484f6c67450656faa32a6294609
changhoyu <tyu9817@naver.com>
2024-07-15 02:17:09 +0900

    post 입력

---
+++ http/1_useEffect/src/Get.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import { useGetData } from './hooks/useGetData';
+
+export default function Get() {
+  // const [isLoading, setIsLoading] = useState(null);
+  // const [fetchedData, setFetchedData] = useState([]);
+  // const [error, setError] = useState(null);
+
+  /**
+   * 기본 useEffect를 사용하는 방법
+   * 단 이럴 경우 보일러플레이트 코드(Boilerplate Code)문제가 발생
+   * 커스텀훅으로 해결 가능하다
+   */
+  // useEffect(() => {
+  //   async function getData() {
+  //     setIsLoading(true);
+  //     try {
+  //       const { data } = await instance.get('posts');
+  //       setFetchedData(data);
+  //     } catch (error) {
+  //       setError({
+  //         message: error.response.message || '데이터를 가져오는데 실패했습니다.',
+  //         status: error.response.status,
+  //       });
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+
+  //   getData();
+  // }, []);
+
+  // 커스텀 훅으로 간결화
+  const { isLoading, error, fetchedData } = useGetData('posts', []);
+
+  if (error) {
+    return (
+      <>
+        <p>{error.status}</p>
+        <p>{error.message}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      {isLoading && <p>로딩중....</p>}
+      {!isLoading && fetchedData.length === 0 && <p>데이터가 없는뎁쇼</p>}
+      {!isLoading && fetchedData.length > 0 && (
+        <ul>
+          {fetchedData.map(item => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
---
+++ http/1_useEffect/src/Post.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import { instance } from './axios/http';
+
+export default function Post() {
+  /**
+   * POST, PATCH, PUT <- 기본적으로 (1)입력을 받고 (2)입력 검증을 함
+   * (1) 이건 할 만 하다
+   * (2) 이게 좀 어렵다
+   *
+   * (1) 입력 받기
+   * 1. 개별 상태
+   * 2. 객체
+   * 3. new FormData()
+   */
+  // 1. 개별 상태
+  // const [title, setTitle] = useState('');
+  // const [body, setBody] = useState('');
+
+  //2 객체
+  // const [input, setInput] = useState({
+  //   title: '',
+  //   body: '',
+  //   userId: 1,
+  // });
+  const [error, setError] = useState(null);
+
+  // 1. 개별 상태
+  // function titleHandler(e) {
+  //   setTitle(e.target.value);
+  // }
+
+  // function bodyHandler(e) {
+  //   setBody(e.target.value);
+  // }
+
+  // 2. 객체
+  // function inputHandler(identifier, e) {
+  //   e.preventDefault();
+  //   setInput(prev => ({
+  //     ...prev,
+  //     [identifier]: e.target.value,
+  //   }));
+  // }
+
+  async function submitHandler(e) {
+    e.preventDefault();
+
+    // 3.new FormData()
+    // 단 input태그에 name 속성이 필요하다
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+    try {
+      const response = await instance.post('posts', data);
+      if (response.status === 201) {
+        alert('성공적으로 데이터가 생성됐습니다.');
+        console.log(response);
+      }
+    } catch (error) {
+      setError({
+        status: error.response.status,
+        message: error.response.message || '데이터생성에실패했습니다.',
+      });
+    }
+    e.target.reset();
+  }
+
+  if (error) {
+    return (
+      <>
+        <p>{error.status}</p>
+        <p>{error.message}</p>
+      </>
+    );
+  }
+  return (
+    <>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="title">title</label>
+          <input id="title" type="text" name="title" />
+        </div>
+        <div>
+          <label htmlFor="body">content</label>
+          <input id="body" type="text" name="body" />
+        </div>
+        <button type="submit">완료</button>
+      </form>
+    </>
+  );
+}
