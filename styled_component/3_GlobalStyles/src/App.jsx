5ff6337ffe370579d3d224d1425ba43eb56f8d45
changhoyu <tyu9817@naver.com>
2024-07-18 14:59:38 +0900

    Globalstyle, reset

--- styled_component/3_GlobalStyles/src/App.jsx
+++ styled_component/3_GlobalStyles/src/App.jsx
@@ -1,7 +1,13 @@
+import { GlobalStyle } from './globalStyle';
+
 function App() {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <GlobalStyle />
+      <h1>h1태그입니다</h1>
+      <p>p태그입니다.</p>
+    </>
+  );
 }
 
 export default App;
